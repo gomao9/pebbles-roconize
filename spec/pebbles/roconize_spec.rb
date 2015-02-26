2ca2f09f916bf25ab10ea468c6b1656ec73df9bc
gomao9 <gomaoq@gmail.com>
2015-02-26 16:43:34 +0900

    ユニットテスト書いた

--- spec/pebbles/roconize_spec.rb
+++ spec/pebbles/roconize_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding : utf-8 -*-
 require 'spec_helper'
 
 describe Pebbles::Roconize do
@@ -5,7 +6,33 @@ describe Pebbles::Roconize do
     expect(Pebbles::Roconize::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'Object#roconizeが定義されている' do
+    expect(Object.new).to respond_to(:roconize)
   end
+
+  it 'Object#deroconizeが定義されている' do
+    expect(Object.new).to respond_to(:deroconize)
+  end
+
+  it 'Object#roconized?が定義されている' do
+    expect(Object.new).to respond_to(:roconized?)
+  end
+
+  it 'Objectは生成時、ロコナイズされていない' do
+    expect(Object.new).not_to be_roconized
+  end
+
+  it 'Object#roconizeは、レシーバをロコナイズする' do
+    obj = Object.new
+    obj.roconize
+    expect(obj).to be_roconized
+  end
+
+  it 'Object#deroconizeは、レシーバをデロコナイズする' do
+    obj = Object.new
+    obj.roconize
+    obj.deroconize
+    expect(obj).not_to be_roconized
+  end
+
 end
